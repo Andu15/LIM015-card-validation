54a8e64517b15e6d36fe56876d671850fd66effb
[name] <estefania_8_3@hotmail.com>
2021-06-14 09:37:39 -0500

    primera version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import validator from './validator.js';
-
 console.log(validator);
+
+const $primerCuadro = document.getElementById("page1"),
+    $segundoCuadro = document.getElementById("page2"),
+    $mensajeDeSalida = document.getElementById("validation"),
+    $logoTarjeta = document.getElementById("claseDeTarjeta");
+
+let $cajaDeNombre = document.getElementById("username"),
+$nombreReemplazado = document.getElementById("name"),
+$numeroDeTarjeta = document.getElementById("numbercreditcard");
+
+document.getElementById("button").addEventListener("click", ()=>{
+    $primerCuadro.style.display = "none";
+    $segundoCuadro.style.display = "";
+    $nombreReemplazado.innerText = $cajaDeNombre.value;
+});
+
+document.getElementById("Enviar").addEventListener("click", () => {
+    let $creditCardNumber = $numeroDeTarjeta.value;
+    if ($creditCardNumber == "") { alert("completa los datos de la tarjeta por favor"); };
+    if ($creditCardNumber == "/^4\d{15}/g") { alert("Tu tarjeta es Visa"); }
+    validator.isValid(creditCardNumber);
+    validator.maskify(creditCardNumber);
+})
+
+
+
+//punto de entrada a un gráfico separado, inicia un nuevo gráfico, que se procesa por separado.
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,18 @@
 const validator = {
-  // ...
+  isvalid: (creditCardNumber) => {
+    let $arrayInverso = [];
+    for (let $d = $creditCardNumber.length - 1; $d >= 0; $d--) {
+      $arrayInverso.push($creditCardNumber[$d]);
+    }
+  },
+  maskify: (creditCardNumber) => { }
 };
 
 export default validator;
+
+/*no puede tener variables en los especificadores de su módulo, porque esas variables aún no tienen valores*/
+/*NO deben existir instrucciones relacionadas al manejo del DOM (addEventListener, GetElementById, InnerHtml). ya que esto es responsabilidad de index.js*/
+/*Las funciones isValid() y maskify() al final deben retornar algo, esto mediante la palabra return*/
+
+/*Cada vez que agreguemos código y vemos que funciona, correr los test (npm test) o por lo menos el pretest (npm run pretest) para que valide sintaxis.
+Hacer commit de cada cambio para tener un histórico, ej: git commit -am "invertí el array de números de la tarjeta". y luego git push origin master.*/
